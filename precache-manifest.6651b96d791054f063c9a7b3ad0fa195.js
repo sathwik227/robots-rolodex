7b4be21578aa5bbdf7d8ac9c3b4a420882f7ae20
sathwik227 <sathwik@jayeesha.com>
2020-10-01 02:06:34 +0530

    Updates

---
+++ precache-manifest.6651b96d791054f063c9a7b3ad0fa195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f624b4df97a43e4819c3628465b229",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "f9e6995e141501153755",
+    "url": "/robots-rolodex/static/css/main.eb489780.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/robots-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e6995e141501153755",
+    "url": "/robots-rolodex/static/js/main.19a6d0bf.chunk.js"
+  },
+  {
+    "revision": "02542f5a8b44e87af7b4",
+    "url": "/robots-rolodex/static/js/runtime-main.07f6b5a4.js"
+  }
+]);
